1f050692df51fe425b30c77a4ed3cf1432af35ee
Geovanni Estevam <geo-estevam@hotmail.com>
2023-10-06 13:50:43 -0700

    commit gb 1:50

--- userapp/App.js
+++ userapp/App.js
@@ -1,78 +1,75 @@
 //import 'core-js/full/symbol/async-iterator';
-import { StatusBar } from 'expo-status-bar';
-import RootNavigator from './src/navigation';
-import { NavigationContainer } from '@react-navigation/native'
-import { Auth } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react-native';
-import AuthContextProvider from './src/contexts/AuthContext';
+import { StatusBar } from "expo-status-bar";
+import RootNavigator from "./src/navigation";
+import { NavigationContainer } from "@react-navigation/native";
+import { Auth } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react-native";
+import AuthContextProvider from "./src/contexts/AuthContext";
 //import UserContextProvider from "./src/contexts/UserContext";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 //import HomeScreen from "./src/screens/HomeScreen";
-import registerNNPushToken from 'native-notify';
-import { PaperProvider } from 'react-native-paper';
+import registerNNPushToken from "native-notify";
+import { PaperProvider } from "react-native-paper";
 import { useEffect, useState } from "react";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 
-
-// Aws Amplify config 
-import { Amplify } from 'aws-amplify';
-import awsExports from './src/aws-exports';
+// Aws Amplify config
+import { Amplify } from "aws-amplify";
+import awsExports from "./src/aws-exports";
 Amplify.configure(awsExports);
 
-
 function App() {
-  // const [hasSignedOut, setHasSignedOut] = useState(false);
-  // useEffect(() => {
-  //   const signOutOnStart = async () => {
-  //     try {
-  //       // Check if the user has already been signed out
-  //       const hasSignedOutFlag = await SecureStore.getItemAsync('hasSignedOut');
-  //       if (!hasSignedOutFlag) {
-  //         await Auth.signOut();
-  //         // Set the flag to indicate that the user has been signed out
-  //         await SecureStore.setItemAsync('hasSignedOut', 'true');
-  //         setHasSignedOut(true);
-  //       }
-  //     } catch (error) {
-  //       console.error('Sign out error:', error);
-  //     }
-  //   };
+  const [hasSignedOut, setHasSignedOut] = useState(false);
+  useEffect(() => {
+    const signOutOnStart = async () => {
+      try {
+        // Check if the user has already been signed out
+        const hasSignedOutFlag = await SecureStore.getItemAsync("hasSignedOut");
+        if (!hasSignedOutFlag) {
+          await Auth.signOut();
+          // Set the flag to indicate that the user has been signed out
+          await SecureStore.setItemAsync("hasSignedOut", "true");
+          setHasSignedOut(true);
+        }
+      } catch (error) {
+        console.error("Sign out error:", error);
+      }
+    };
 
-  //   signOutOnStart();
-  // }, []);
+    signOutOnStart();
+  }, []);
 
-  // useEffect(() => {
-  //   const checkCurrentUser = async () => {
-  //     try {
-  //       const user = await Auth.currentAuthenticatedUser();
-  //       // User is authenticated, navigate to the main app screen
-  //       // You can use navigation to the appropriate screen here
-  //       console.log('User is already authenticated:', user);
-  //     } catch (error) {
-  //       // No user is authenticated, show authentication screens
-  //       console.log('No user is authenticated:', error);
-  //     }
-  //   };
+  useEffect(() => {
+    const checkCurrentUser = async () => {
+      try {
+        const user = await Auth.currentAuthenticatedUser();
+        // User is authenticated, navigate to the main app screen
+        // You can use navigation to the appropriate screen here
+        console.log("User is already authenticated:", user);
+      } catch (error) {
+        // No user is authenticated, show authentication screens
+        console.log("No user is authenticated:", error);
+      }
+    };
 
-  //   checkCurrentUser();
-  // }, []);
+    checkCurrentUser();
+  }, []);
 
-  registerNNPushToken(12497, 'wDb6oKTWDGkDZd1Rv468rP');
+  registerNNPushToken(12497, "wDb6oKTWDGkDZd1Rv468rP");
   return (
     <NavigationContainer>
       <AuthContextProvider>
         {/* <UserContextProvider> */}
-          <GestureHandlerRootView style={{ flex: 1 }}>
-            <PaperProvider>
-              <RootNavigator />
-            </PaperProvider>
-          </GestureHandlerRootView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <PaperProvider>
+            <RootNavigator />
+          </PaperProvider>
+        </GestureHandlerRootView>
         {/* </UserContextProvider> */}
-        <StatusBar style="light"/>
+        <StatusBar style="light" />
       </AuthContextProvider>
     </NavigationContainer>
   );
-};
+}
 
 export default withAuthenticator(App);
-
