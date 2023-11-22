14c7d31f5844029fd4a7c2a70c3c06f02041102b
Geovanni Estevam <geo-estevam@hotmail.com>
2023-11-22 14:04:46 -0800

    commit gb 22/11/2023

--- dashboard/src/pages/RoutesPage.jsx
+++ dashboard/src/pages/RoutesPage.jsx
@@ -24,29 +24,67 @@ function RoutesPages() {
     setAllKids(newOrder);
   }
 
+  function handleOnDragVanBoxEnd(result) {
+    // console.log("drag drops event", result);
+    // console.log("destination", result.destination);
+
+    if (!result.destination) return;
+    const newOrder = Array.from(kidsOnVan);
+    const [draggedItem] = newOrder.splice(result.source.index, 1);
+    console.log(draggedItem);
+    newOrder.splice(result.destination.index, 0, draggedItem);
+    setKidsOnVan(newOrder);
+  }
+
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchVansData = async () => {
       try {
         const vansResponse = await API.graphql(
           graphqlOperation(listVans, { limit: 100 })
         );
         const vansData = vansResponse.data.listVans.items;
         setVans(vansData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-        const kidsResponse = await API.graphql({
+    const fetchKidsWithoutVan = async () => {
+      try {
+        const variables = {
+          filter: { vanID: { eq: "" } },
+        };
+        const kidsWithoutVan = await API.graphql({
           query: listKids,
-          variables: {
-            filter: { vanID: { ne: null } },
-          },
+          variables: variables,
         });
-        const kidsData = kidsResponse.data.listKids.items;
+        const kidsData = kidsWithoutVan.data.listKids.items;
+        console.log("kidsData without van", kidsData);
         setAllKids(kidsData);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    fetchData();
+    const fetchKidsInVan = async () => {
+      try {
+        const variables = {
+          filter: { vanID: { ne: "" } },
+        };
+        const kidsInVan = await API.graphql({
+          query: listKids,
+          variables: variables,
+        });
+        const kidsData = kidsInVan.data.listKids.items;
+        console.log("kidsData with van", kidsData);
+        setKidsOnVan(kidsData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchVansData();
+    fetchKidsWithoutVan();
+    fetchKidsInVan();
   }, []);
 
   return (
@@ -62,7 +100,7 @@ function RoutesPages() {
                 className="kids"
               >
                 {allKids
-                  .filter((kid) => !kid.vanID)
+                  //.filter((kid) => !kid.vanID)
                   .map((kid, index) => (
                     <Draggable
                       key={kid.id}
@@ -89,30 +127,39 @@ function RoutesPages() {
             )}
           </Droppable>
         </div>
-
-        <div className="vans-list">
-          <h2>All Vans</h2>
-          <div className="vans">
-            {vans.map((van) => (
-              <div key={van.id} className="van-item">
-                <h3>{van.name}</h3>
-                <div className="kids-in-van">
-                  {allKids
-                    .filter((kid) => kid.vanID === van.id)
-                    .map((kid) => (
-                      <div key={kid.id} className="kid-item">
-                        <div className="kid-name">{kid.name}</div>
-                        <div className="drop-off-address">
-                          {kid.dropOffAddress}
-                        </div>
-                      </div>
-                    ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
       </DragDropContext>
+      <div className="vans-list">
+        <h2>All Vans</h2>
+        <DragDropContext onDragEnd={handleOnDragVanBoxEnd}>
+          <Droppable droppableId="vansBox" type="group" direction="vertical">
+            {(provided) => (
+              <div
+                className="vans"
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {vans.map((van) => (
+                  <div key={van.id} className="van-item">
+                    <h3>{van.name}</h3>
+                    <div className="kids-in-van">
+                      {kidsOnVan
+                        //.filter((kid) => kid.vanID === van.id)
+                        .map((kid) => (
+                          <div key={kid.id} className="kid-item">
+                            <div className="kid-name">{kid.name}</div>
+                            <div className="drop-off-address">
+                              {kid.dropOffAddress}
+                            </div>
+                          </div>
+                        ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
   );
 }
