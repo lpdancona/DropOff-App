46aff2c8f0807d2350bbaa8aa82b74b619f8a732
Leonardo Picchi Dancona <lpdancona@gmail.com>
2024-01-15 21:50:08 -0800

    added upload image to storage

--- userapp/src/screens/PickScreen/PickScreen.js
+++ userapp/src/screens/PickScreen/PickScreen.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import { Auth, API, graphqlOperation } from "aws-amplify";
+import { Auth, API, graphqlOperation, Storage } from "aws-amplify";
 import { GetKidByParentEmail } from "../../graphql/queries";
-import * as ImagePicker from "expo-image-picker"; // Import ImagePicker from Expo
+import { UpdateKid } from "../../graphql/mutations";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { Blob } from "expo-blob";
 
 import styles from "./styles";
 
 const PickScreen = () => {
-  const [kid, setKid] = useState(null);
+  const [kid, setKid] = useState([]);
 
   useEffect(() => {
     const fetchKidData = async () => {
@@ -31,6 +34,35 @@ const PickScreen = () => {
     fetchKidData();
   }, []);
 
+  const uploadImage = async (uri, kidID) => {
+    try {
+      if (!uri) {
+        throw new Error("Image URI is undefined");
+      }
+
+      console.log("Fetching image from:", uri);
+      const response = await fetch(uri);
+      const arrayBuffer = await response.arrayBuffer();
+      const blob = new Blob([arrayBuffer]);
+
+      console.log("Uploading image to S3...");
+      const filename = `kid-photo-${kidID}-${Date.now()}`;
+      await Storage.put(filename, blob, {
+        contentType: "image/jpeg",
+      });
+
+      console.log("Image uploaded successfully.");
+
+      const imageURL = await Storage.get(filename);
+      console.log("Image URL:", imageURL);
+
+      return imageURL;
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      throw error;
+    }
+  };
+
   const openImageLibrary = async () => {
     try {
       const permissionResult =
@@ -47,10 +79,42 @@ const PickScreen = () => {
         aspect: [4, 3],
         quality: 1,
       });
+      console.log("ImagePicker Result:", result);
 
       if (!result.cancelled) {
-        console.log("Selected Image:", result.uri);
-        // You can do something with the selected image, such as uploading it.
+        if (result.assets && result.assets.length > 0) {
+          const imageURL = await uploadImage(result.assets[0].uri, kid[0].id);
+
+          console.log("Selected Image:", imageURL);
+
+          // Update the kid's photo in the state
+          setKid((prevKids) => {
+            const updatedKids = prevKids.map((kidItem) => {
+              if (kidItem.id === kid[0].id) {
+                return {
+                  ...kidItem,
+                  photo: imageURL,
+                };
+              } else {
+                return kidItem;
+              }
+            });
+
+            return updatedKids;
+          });
+
+          // Update the kid's photo in the GraphQL API
+          await API.graphql(
+            graphqlOperation(UpdateKid, {
+              input: {
+                id: kid[0].id,
+                photo: imageURL,
+              },
+            })
+          );
+        } else {
+          console.error("Image assets not found in the result.");
+        }
       }
     } catch (error) {
       console.error("Error picking image:", error);
@@ -66,7 +130,12 @@ const PickScreen = () => {
             <View key={kidItem.id}>
               <Text>Name: {kidItem.name}</Text>
               <Text>Drop-off Address: {kidItem.dropOffAddress}</Text>
-              {/* Add more Kid details as needed */}
+              {kidItem.photo && (
+                <Image
+                  style={{ width: 100, height: 100 }}
+                  source={{ uri: kidItem.photo }}
+                />
+              )}
             </View>
           ))}
           <TouchableOpacity onPress={openImageLibrary}>
